90027252fe86ed328fc53283d247665984d460e7
milandjuric99 <milan.djuric.019@gmail.com>
2021-10-13 16:01:14 +0200

    first commit

---
+++ webshop.parent/frontend/src/main/java/webshop/frontend/controller/MainController.java
@@ -0,0 +1,13 @@
+package webshop.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping(value = "")
+    public String viewHomePage(){
+        return "index";
+    }
+}
